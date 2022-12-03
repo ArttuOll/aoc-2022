7c6ff40a1113b008ceed7dc9016c68b62067e678
Arttu Olli <60509537+ArttuOll@users.noreply.github.com>
2022-12-03 13:36:23 +0200

    Solve day 1 part 2

---
+++ src/day1/b.ts
@@ -0,0 +1,26 @@
+import { readInput } from "../util";
+import { map, reduce, sort, takeLeft } from "fp-ts/ReadonlyArray";
+import { flow } from "fp-ts/function";
+import { split } from "fp-ts/string";
+import { Ord as OrdNumber } from "fp-ts/number";
+import { reverse } from "fp-ts/Ord";
+
+const sum = reduce<number, number>(
+  0,
+  (value, accumulator) => value + accumulator
+);
+const stringToNumber = map(Number);
+const inputToElfCalorieArrays = flow(
+  split("\n\n"),
+  map(flow(split("\n"), stringToNumber))
+);
+
+const result = flow(
+  inputToElfCalorieArrays,
+  map(sum),
+  sort(reverse(OrdNumber)),
+  takeLeft(3),
+  sum
+)(readInput(1));
+
+console.log(result);
