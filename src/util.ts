e377819dd94db65717ea4efe253a52fbfc4e5f77
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2022-12-09 10:54:03 +0200

    Make readFile use current project src path

--- src/util.ts
+++ src/util.ts
@@ -3,8 +3,5 @@ import { IO } from "fp-ts/IO";
 
 export function readInput(day: number): IO<string> {
   return () =>
-    readFileSync(
-      `/home/arttu/ohjelmistoprojektit/aoc-2022/src/day${day}/input.txt`,
-      { encoding: "utf-8" }
-    );
+    readFileSync(`${__dirname}/day${day}/input.txt`, { encoding: "utf-8" });
 }
